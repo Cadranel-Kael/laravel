fdb4fa22d3a7aa1ae3cc20c55d6d9f2dc91ff8da
dominiquevilain <dominique.vilain@hepl.be>
2023-02-10 15:19:15 +0100

    2 - PHPUnit

--- index.php
+++ index.php
@@ -1,53 +1,28 @@
 <?php
-
-$author = $_GET["author"];
+$author = $_GET["author"] ?? "";
 
 $books = [
-	[
-		"title" => "L’étranger",
-		"author" => "Albert Camus"
-	],
-	[
-		"title" => "La peste",
-		"author" => "Albert Camus"
-	],
-	[
-		"title" => "La conjuration des imbéciles",
-		"author" => "John Kennedy Toole"
-	]
+    [
+        "title" => "L’étranger",
+        "author" => "Albert Camus",
+        "release_date" => 1942,
+    ],
+    [
+        "title" => "La peste",
+        "author" => "Albert Camus",
+        "release_date" => 1947,
+    ],
+    [
+        "title" => "La conjuration des imbéciles",
+        "author" => "John Kennedy Toole",
+        "release_date" => 1980,
+    ],
 ];
 
-$filteredBooks = [];
+$decision = function (array $item) use ($author): bool {
+    return $item["author"] === $author;
+};
 
-foreach ($books as $book) {
-	if ($book["author"] === $author) {
-		$filteredBooks[] = $book;
-	}
-}
+$filteredBooks = array_filter($books, $decision);
 $authors = array_column($books, "author", "author");
-?>
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-	<meta charset="UTF-8">
-	<title></title>
-</head>
-
-<body>
-	<h1>Ma collection de livres</h1>
-	<?php foreach ($filteredBooks as $book) : ?>
-		<article>
-			<h2><?= $book["title"] ?></h2>
-			<p>Écrit par : <?= $book["author"] ?></p>
-		</article>
-	<?php endforeach ?>
-	<nav>
-		<h2>Les auteurs</h2>
-		<?php foreach ($authors as $bookAuthor) : ?>
-			<a href="/?author=<?= $bookAuthor ?>"><?= $bookAuthor ?></a>
-		<?php endforeach ?>
-	</nav>
-</body>
-
-</html>
+require "index.view.php";
